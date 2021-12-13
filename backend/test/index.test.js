c7199857fc7fd30b4ba3e41a43128b20eecbe24b
jfarroyo291 <j-a-m291@hotmail.com>
2021-12-13 13:37:04 -0500

    test(Test): Add index and user tests

--- backend/test/index.test.js
+++ backend/test/index.test.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const app = require('../routes');
 const expect = require('chai').expect;
 const assert = require('chai').assert;
@@ -11,4 +12,19 @@ describe("Server a live message",
             .expect(200, {"message":"pong"})
         })
     }
+=======
+const app = require('../routes');
+const expect = require('chai').expect;
+const assert = require('chai').assert;
+const request = require('supertest');
+
+describe("Server a live message",
+    ()=>{
+        it("Ping-pong", ()=>{
+            return request(app)
+            .get("/api")
+            .expect(200, {"message":"pong"})
+        })
+    }
+>>>>>>> 73ec1c64de61a20f2c8896c1ffb4b60a0b19c786
 )
